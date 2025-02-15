83bf0c8e6184b9fe815a851b7932d31883357acd
diazdon07 <103618197+diazdon07@users.noreply.github.com>
2025-02-15 20:22:22 +0800

    Delete qualityCheck.js

--- qualityCheck.js
+++
@@ -1,8 +0,0 @@
-const clickButton = document.getElementById("Qcheck");
-
-clickButton.addEventListener("click", function () {
-    const isClick = clickButton.value || "default"; // Use a default value if not set
-    chrome.storage.sync.set({ clickState: isClick }, () => {
-        console.log("clickState updated to:", isClick);
-    });
-});
