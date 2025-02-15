c5c10f15283fc9cac68c9d53ac29221d06ac485f
diazdon07 <103618197+diazdon07@users.noreply.github.com>
2025-02-15 20:24:05 +0800

    Update on_run.js

--- on_run.js
+++ on_run.js
@@ -2,18 +2,6 @@ const site = window.location.hostname;
 const path = window.location.pathname;
 const sitePath = path.split('/')[1] || "";
 
-const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
-
-function Create_Custom_Element(tag, attr_tag, attr_name, value) {
-    const custom_element = document.createElement(tag);
-    custom_element.setAttribute(attr_tag, attr_name);
-    custom_element.innerHTML = value;
-    document.body.append(custom_element);
-}
-
-// console.log(site);
-// console.log(sitePath);
-
 if (site.includes("webbuilder.localsearch.com.au")) {
 
     switch (sitePath) {
